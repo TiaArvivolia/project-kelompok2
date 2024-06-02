25bad2d16c669c7591c9c310ae20ee7b9eda4bc9
TiaArvivolia <arvivoliatia@gmail.com>
2024-06-02 20:36:26 +0700

    dashboard

--- home.php
+++ home.php
@@ -6,15 +6,19 @@ include_once('model/session.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-    <!-- Include your CSS and JS files here -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 
-<body>
+<body class="hold-transition sidebar-mini">
     <!-- Site wrapper -->
     <div class="wrapper">
         <!-- Navbar -->
@@ -25,59 +29,99 @@ include_once('model/session.php');
         <?php include('layouts/sidebar.php') ?>
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
+
             <!-- Content Header (Page header) -->
-            <section class="content-header">
+            <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Selamat Datang</h1>
+                            <h1 class="m-0">Dashboard</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Blank Page</li>
+                                <li class="breadcrumb-item active">Dashboard</li>
                             </ol>
                         </div>
                     </div>
-                </div><!-- /.container-fluid -->
-            </section>
+                </div>
+            </div>
+            <!-- /.content-header -->
 
             <!-- Main content -->
             <section class="content">
-                <!-- Default box -->
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Title</h3>
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- All Info Boxes in Two Rows -->
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-comments"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Dosen</span>
+                                    <span class="info-box-number">123</span>
+                                </div>
+                            </div>
+                        </div>
 
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i class="fas fa-times"></i>
-                            </button>
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-user-graduate"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Mahasiswa</span>
+                                    <span class="info-box-number">456</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-tie"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Tendik</span>
+                                    <span class="info-box-number">789</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user-friends"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Alumni</span>
+                                    <span class="info-box-number">101</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <?php
-                        if (isset($_SESSION['success'])) {
-                            echo "<div class='alert alert-success'>" . $_SESSION['success'] . "</div>";
-                            unset($_SESSION['success']);
-                        }
-                        ?>
-                        Start creating your amazing application!
+
+                    <div class="row">
+                        <!-- Second Row -->
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-child"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Orang Tua</span>
+                                    <span class="info-box-number">112</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-secondary elevation-1"><i class="fas fa-industry"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Jawaban Industry</span>
+                                    <span class="info-box-number">131</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <!-- /.card-body -->
-                    <div class="card-footer">
-                        Footer
-                    </div>
-                    <!-- /.card-footer-->
                 </div>
-                <!-- /.card -->
             </section>
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
+
         <?php include('layouts/footer.php') ?>
 
         <!-- Control Sidebar -->
